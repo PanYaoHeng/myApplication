12b360231e361073485318601c49f6d4186588d3
潘耀恒 <pyh_work@163.com>
2020-01-12 22:01:38 +0800

    打开窗口时屏蔽滚动

--- src/components/basic/Modal/index.tsx
+++ src/components/basic/Modal/index.tsx
@@ -3,6 +3,7 @@ import { render, unmountComponentAtNode } from 'react-dom';
 import './index.less';
 import IconButton from '../IconButton';
 import { Button, ButtonTypes } from '../Button';
+import { getScrollbarWidth } from '../../../tools/htmlTools';
 
 let modalRootContainer = document.getElementById('modal-root');
 if (!modalRootContainer) {
@@ -24,6 +25,9 @@ interface Props {
 export const Modal: React.FC<Props> = (props) => {
     const { children, showHeader, showFooter, onClose, title, onOK, okText, cancelText } = props;
     useEffect(() => {
+        document.body.style.overflow = 'hidden';
+        document.body.style.width = `calc(100% - ${getScrollbarWidth()}px)`;
+
         const container = document.createElement('div');
         container.classList.add('modal-container');
         container.classList.add('vertical-center');
@@ -35,32 +39,42 @@ export const Modal: React.FC<Props> = (props) => {
         });
         (modalRootContainer as HTMLElement).appendChild(container);
 
+        const header = showHeader ? (
+            <div className="modal-header vertical-center">
+                <span>{title}</span>
+                <IconButton iconName="close" onClick={onClose} />
+            </div>
+        ) : null;
+
+        const footer = showFooter ? (
+            <div className="modal-footer">
+                <Button onClick={onClose}>{cancelText}</Button>
+                <Button className="confirm-btn" onClick={onOK} type={ButtonTypes.Primary}>
+                    {okText}
+                </Button>
+            </div>
+        ) : null;
+
         const component = (
-            <div className="modal-content">
-                {showHeader && (
-                    <div className="modal-header vertical-center">
-                        <span>{title}</span>
-                        <IconButton iconName="close" onClick={onClose} />
-                    </div>
-                )}
+            <div className="modal-content" tabIndex={0}>
+                {header}
                 <div className="modal-body">{children}</div>
-                {showFooter && (
-                    <div className="modal-footer">
-                        <Button onClick={onClose}>{cancelText}</Button>
-                        <Button className="confirm-btn" onClick={onOK} type={ButtonTypes.Primary}>
-                            {okText}
-                        </Button>
-                    </div>
-                )}
+                {footer}
             </div>
         );
-        render(component, container);
+
+        render(component, container, () => {
+            (container.firstChild as HTMLElement).focus();
+        });
 
         return () => {
             unmountComponentAtNode(container);
             container.remove();
+            document.body.style.overflow = '';
+            document.body.style.width = '';
         };
     }, [children, showHeader, showFooter]);
+
     return null;
 };
 Modal.defaultProps = {
